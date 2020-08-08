baee3ce9632927c1d22655f1fe13cf6e78ff35b8
o0pmitev <pmitev89@gmail.com>
2020-08-08 17:42:37 +0200

    Header animation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from 'react'
 import './App.scss'
 import headerImg from './assets/starwars.png'
 import Species from './components/Species/Species'
-
+import walker from './assets/pngegg.png'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <div className="box">
+          <img class="character" src={walker}></img>
+        </div>
         <img className="header-img" src={headerImg}></img>
+        <div className="box">
+          <img class="character" src={walker}></img>
+        </div>
       </header>
+
       <Species />
     </div>
   );
--- src/components/Species/SpeciesCard.js
+++ src/components/Species/SpeciesCard.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 const SpeciesCard = ({info}) => {
   console.log(info)
-  
+
   const jsx = info ?  info.map((el, index) => (
       <div key={index}>
         <h3>{el.name}</h3>
+        <p><b>Classification: </b> {el.classification}</p>
+        <p><b>Designation: </b> {el.designation}</p>
+        <hr />
       </div>
     )) : null
   
